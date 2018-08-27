b18b5d1e3da22ce7d686a6441ee16935ed68b473
Peter Strøiman <peter@stroiman.com>
2018-08-27 18:25:31 +0200

    Working email application

--- test-app-2/client/src/App.js
+++ test-app-2/client/src/App.js
@@ -5,31 +5,57 @@ class App extends Component {
   constructor() {
     super()
     this.click = this.click.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.state = { email: ""};
+    this.status = "";
   }
 
   click(e) {
-    console.log("CLICK!");
     e.preventDefault();
-    const body = { email: "john.doe@example.com" };
+    const body = { email: this.state.email };
     fetch("/api/signup", {
       "method": "POST",
       "headers": {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(body)
+    }).then(response => {
+      if (response.ok) {
+        this.setState({status: "OK"});
+      } else {
+        response.json().then(json => {
+          const errorMsg = json.error;
+          this.setState({status: "ERROR", errorMsg });
+        })
+      }
     });
+  }
+
+  onChange(e) {
+    this.setState({email: e.target.value});
+  }
+
+  renderStatus() {
+    switch(this.state.status) {
+      case "OK": return <div className="alert alert-success">Signup success</div>
+      case "ERROR": return (<div className="alert alert-danger">Error signing up<br />{this.state.errorMsg}</div>)
+      default: return null;
+    }
   }
 
   render() {
     return (
-      <div>
+      <div className="bg-dark">
         <div style={{position: "relative"}}>
-          <div className="navbar navbar-light">
+          <nav className="navbar bg-light">
             Super app, Please sign up!
+          </nav>
+          <div className="container mt-4">
+            { this.renderStatus() }
           </div>
         </div>
 
-        <div className="bg-dark" style={{
+        <div style={{
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
@@ -41,7 +67,10 @@ class App extends Component {
               <form>
                 <div className="form-group">
                   <label htmlFor="email" className="text-light">E-mail:</label>
-                  <input className="form-control" type="text" name="email" />
+                  <input className="form-control" type="text" name="email"
+                    onChange={this.onChange}
+                    value={this.state.email}
+                  />
                 </div>
                 <div className="form-group">
                   <button className="btn btn-primary form-control"
--- test-app-2/server/register-user.js
+++ test-app-2/server/register-user.js
@@ -1,5 +1,39 @@
+const nodemailer = require('nodemailer');
+
+const { SMTP_HOST, SMTP_PORT } = process.env;
+
+if (!SMTP_HOST) {
+  console.error("Missing SMTP_HOST env variable");
+  process.exit(1);
+};
+if (!SMTP_PORT) {
+  console.error("Missing SMTP_PORT env variable")
+  process.exit(1);
+};
+
+let transporter = nodemailer.createTransport({
+  host: SMTP_HOST,
+  port: SMTP_PORT,
+  secure: false,
+});
+
 const registerUser = async email => {
-  throw new Error("Not implemented yet");
+  let mailOptions = {
+    from: '"Docker Test Application" <sender@example.com>',
+    to: email,
+    subject: 'Email confirmation',
+    text: 'Thank you for signing up. Click somewhere to confirm your e-mail address',
+  };
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending mail", error);
+        return reject(error);
+      }
+      console.log("Message sent: %s", info.messageId);
+      resolve();
+    });
+  });
 }
 
 module.exports = registerUser;
