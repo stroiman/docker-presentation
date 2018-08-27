89e1a8b607acef44a7db2cc1fc909fcb57b8af18
Peter Strøiman <peter@stroiman.com>
2018-08-27 18:29:16 +0200

    Add build to server application

--- test-app-2/server/server.js
+++ test-app-2/server/server.js
@@ -2,10 +2,9 @@ const express = require("express");
 const api = require("./api");
 
 const app = express();
-app.get("/", (req,res) => {
-  res.send("Hello");
-});
 
 app.use("/api", api);
+
+app.use(express.static(__dirname + "/build"));
 
 app.listen(9000);
